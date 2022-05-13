d9084824e0a375af7a9711a00f227a257bbb5650
Pedro J. Pérez <pedro.j.perez@uv.es>
2022-05-13 20:25:55 +0200

    Initial commit

---
+++ materiales/pkgs_a_instalar_sfpie_2022.R
@@ -0,0 +1,12 @@
+#- paquetes a instalar (curso R, SFPIE, 2022)
+
+install.packages("tidyverse")
+install.packages("remotes")
+install.packages("ggthemes")
+remotes::install_github("perezp44/pjpv.curso.2022")
+install.packages("sf")
+
+pkgs <- c("rio", "performance", "see", "meme", "tmap")
+
+
+pkgs_2 <- c("memer")
